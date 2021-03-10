91b39ba0b88838a6e39b0f207de946d045cb47f6
Kristen Ruprecht <ksruprec@gmail.com>
2021-03-09 20:02:02 -0500

    added files

--- week-3/day-1/script.js
+++ week-3/day-1/script.js
@@ -3,3 +3,15 @@ function closeSplash() {
   document.querySelector("#splashPage").style.visibility = "hidden";
   document.querySelector("#tableOfContent").style.visibility = "visible";
 }
+
+var myArray = ["hello", 2];
+console.log(myArray);
+
+var myObject = {
+  firstName: 'Kristen', 
+  lastName: 'Ruprecht', 
+  favoriteColor: 'green'
+}
+
+myObject.favoriteColor = 'blue';
+console.log(favoriteColor);
